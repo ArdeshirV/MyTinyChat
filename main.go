43a1f0a6c4f60a9183f7a7203fa66554bf76ce65
ArdeshirV <e-job@protonmail.com>
2025-09-01 17:13:31 +0200

    Add receiveMessages(conn net.Conn)

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 )
 
 func main() {
 	fmt.Println(Prompt("My Tiny Chat"))
 }
+
+func receiveMessages(conn net.Conn) {
+  reader := bufio.NewReader(conn)
+  for {
+    message, err := reader.ReadString('\n')
+    if err != nil {
+      break
+    }
+    fmt.Printf("Client> %s", message)
+  }
+}
+
+
