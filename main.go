2d5066e9114715e2d95dd34f10b023009ae615d2
ArdeshirV <e-job@protonmail.com>
2025-09-01 17:36:41 +0200

    Add main functionality

--- main.go
+++ main.go
@@ -3,11 +3,41 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log/slog"
 	"net"
+	"os"
+
+	"log"
 )
 
 func main() {
 	fmt.Println(Prompt("My Tiny Chat"))
+  ln, err := net.Listen("tcp", ":8080")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer ln.Close()
+
+  slog.Info("Server started. Listen on port 8080")
+
+  conn, err := ln.Accept()
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer conn.Close()
+
+  go receiveMessages(conn)
+  scanner := bufio.NewScanner(os.Stdin)
+  for {
+    fmt.Print("Server> ")
+    scanner.Scan()
+    message := scanner.Text()
+    if message == "/quit" {
+      break
+    }
+    sendMessage(conn, message)
+  }
+  slog.Info("Server closed")
 }
 
 func receiveMessages(conn net.Conn) {
@@ -24,3 +54,4 @@ func receiveMessages(conn net.Conn) {
 func sendMessage(conn net.Conn, message string) {
   conn.Write([]byte(message + "\n"))
 }
+
